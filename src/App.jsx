1123d15630c7ae68ade118c3854a63c2ad356b5a
mabdullahjs <mabdullah2037@outlook.com>
2024-09-29 11:28:25 +0500

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, sapiente!</p>
+      <ul>
+        <li>abc</li>
+        <li>abc</li>
+        <li>abc</li>
+        <li>abc</li>
+      </ul>
+    </>
+  )
+}
